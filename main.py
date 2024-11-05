b82c6d45f27477b75f217b283a3642495fe15c3d
Bruno Ancona <brunoanconasala@gmail.com>
2024-11-04 22:48:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import time
 import datetime
 
 # URL de la página estática
-URL_BASE = "https://brunoancona.me"
+URL_BASE = "http://localhost:8080"
 
 # Clase para los datos de PostgreSQL
 class DelegacionSM(SQLModel, table=True): # type: ignore
