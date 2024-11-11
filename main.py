cb15bbc5f599b1b84c9c4a4533887ef611bfe2e1
Bruno Ancona <brunoanconasala@gmail.com>
2024-11-11 11:04:01 -0600

    Usar URL temporal

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import datetime
 
 
 # URL de la página estática
-URL_BASE = "https://smmun.com"
+URL_BASE = "https://jellyfish-app-iyb7k.ondigitalocean.app"
 
 
 # Clase para recibir y validar el forms de delegaciones
@@ -192,7 +192,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost",
         "http://localhost:8080",
-        "https://smmun.com"
+        "https://smmun.com",
+        "https://jellyfish-app-iyb7k.ondigitalocean.app"
     ],
     allow_credentials=True,
     allow_methods=["POST"],
